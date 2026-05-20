39a40ff146ac117682b40499eb7f1ea435de9030
RONALD JAMES_MCA_A_2024-26_38_UEMK <ronald.james.3110@gmail.com>
2026-05-20 11:24:37 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-app/',  // ← your repo name here
+  base: '/Weather-App-React-/',
   server: {
     proxy: {
       '/api': {
